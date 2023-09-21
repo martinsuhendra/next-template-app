4535df040593b5f213c81802182dedc5f1759570
martinsuhendra <suhendra.martin90@gmail.com>
2023-09-21 21:46:18 +0800

    ci: refactor prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isDeploying = process.env.DEPLOYING_MODE === 'true'
 const isProd = process.env.NODE_ENV === 'production'
-const REPO_URL = 'next-template-app'
+const REPO_URL = '/next-template-app'
 
 const nextConfig = {
   output: isDeploying ? 'export' : undefined,
