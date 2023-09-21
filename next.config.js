4212e5971586925eb041073b9ab6cff406714ff5
martinsuhendra <suhendra.martin90@gmail.com>
2023-09-21 20:30:56 +0800

    ci: change basepath name

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const envImageUnoptimize = process.env.NODE_ENV === 'production' ? true : false
+const REPO_NAME = '/next-template-app'
 
 const nextConfig = {
   output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
-  basePath: process.env.NODE_ENV === 'production' ? '/github-pages' : undefined,
+  basePath: process.env.NODE_ENV === 'production' ? REPO_NAME : undefined,
   images: {
     unoptimized: envImageUnoptimize,
   },
