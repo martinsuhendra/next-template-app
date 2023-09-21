06f06bb0f8bbaf3aea6c35923c5050d441012d13
martinsuhendra <suhendra.martin90@gmail.com>
2023-09-21 21:34:37 +0800

    ci: add deploying mode to run export output

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isStandalone = process.env.STANDALONE_MODE === 'true'
+const isDeploying = process.env.DEPLOYING_MODE === 'true'
 const isProd = process.env.NODE_ENV === 'production'
 const envImageUnoptimize = isProd ? true : false
 
 const nextConfig = {
-  output: isStandalone,
+  output: isDeploying ? 'export' : undefined,
   assetPrefix: isProd ? '/next-template-app' : undefined,
   images: {
     unoptimized: envImageUnoptimize,
