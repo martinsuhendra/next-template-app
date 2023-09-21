c8b57a5405add13dc0772ba948827972e7ab0cbd
martinsuhendra <suhendra.martin90@gmail.com>
2023-09-21 19:56:02 +0800

    ci: add /github-pages as a base path

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const envImageUnoptimize = process.env.NODE_ENV === 'production' ? true : false
 
 const nextConfig = {
   output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  basePath: process.env.NODE_ENV === 'production' ? '/github-pages' : undefined,
   images: {
     unoptimized: envImageUnoptimize,
   },
