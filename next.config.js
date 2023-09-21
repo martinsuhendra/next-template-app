7450cb4357a163ef9967f7816c84b613548922e3
martinsuhendra <suhendra.martin90@gmail.com>
2023-09-21 21:27:38 +0800

    ci: use assetPrefix instead of basepath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-const envImageUnoptimize = process.env.NODE_ENV === 'production' ? true : false
-const REPO_NAME = '/next-template-app'
+const isStandalone = process.env.STANDALONE_MODE === 'true'
+const isProd = process.env.NODE_ENV === 'production'
+const envImageUnoptimize = isProd ? true : false
 
 const nextConfig = {
-  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
-  basePath: process.env.NODE_ENV === 'production' ? REPO_NAME : undefined,
+  output: isStandalone,
+  assetPrefix: isProd ? '/next-template-app' : undefined,
   images: {
     unoptimized: envImageUnoptimize,
   },
