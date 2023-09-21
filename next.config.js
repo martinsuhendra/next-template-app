27ecdfd56e640aafa1d29bb0998c662af469c548
martinsuhendra <suhendra.martin90@gmail.com>
2023-09-21 19:42:38 +0800

    ci: config nextjs config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const envImageUnoptimize = process.env.NODE_ENV === 'production' ? true : false
+
+const nextConfig = {
+  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  images: {
+    unoptimized: envImageUnoptimize,
+  },
+}
 
 module.exports = nextConfig
